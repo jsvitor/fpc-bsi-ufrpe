8a9f00a5e008d855904fa564ce1a028960238e56
Vitor <jszvitor@gmail.com>
2021-09-14 09:25:14 -0300

    refactor(main): added and removed comments, and code refactor

--- 1 - complexity/main.py
+++ 1 - complexity/main.py
@@ -8,74 +8,64 @@ def sum_value_of_complexity(array):
     elif i == 'PROCSUM': amount += 1
     
     elif i == 'PROCMULT': amount += 10
-    
+
   return amount
 
 def complexity_verify(array):
-  amount = 0; # complexity in time unity
-  # i is a instruction of the algorithm
-  new_array = array.copy()
-  # stack = []
-  rep_for = 0
+  amount = 0; # amount of algorithm complexity by time unity
+  # The "array" is a list and has a set of algorithm instructions where the complexity will be calculated
+  # example:
+  #         input --> array = ['INICIO', 'IO', 'LOOP', '10', 'PROCSUM', 'FIMLOOP', 'FIM']
+  #         output -> 40
   if 'INICIO' in array[0]:
-    interaction = 0
-    while interaction < len(array):
-      if array[interaction] == 'FIM': break
+    instruction = 0
+    # scrolls through the list analyzing each operation
+    while instruction < len(array):
+      if array[instruction] == 'FIM': break
 
-      elif array[interaction] == 'INICIO':
-        interaction += 1
-      elif array[interaction] == 'FIMLOOP':
-        interaction += 1
-
-      elif array[interaction] == 'IO':
-        # stack.append('30')
+      elif array[instruction] == 'INICIO': instruction += 1
+      
+      elif array[instruction] == 'FIMLOOP': instruction += 1
+      
+      elif array[instruction] == 'IO':
         amount += 30
-        interaction += 1
-      
-      elif array[interaction] == 'MEM':
-        # new_array.pop( new_array.index(i) )
-        amount += 10
-        interaction += 1
+        instruction += 1
 
-      elif array[interaction] == 'PROCSUM':
-        #new_array.pop( new_array.index(i) )
+      elif array[instruction] == 'MEM':
+        amount += 10
+        instruction += 1
+
+      elif array[instruction] == 'PROCSUM':
         amount += 1
-        interaction += 1
-      
-      elif array[interaction] == 'PROCMULT':
-        # new_array.pop( new_array.index(i) )
-        amount += 10
-        interaction += 1
+        instruction += 1
 
-      elif array[interaction] == 'LOOP':
-        position = interaction + 1
-        repetition = int(new_array[position])
-        # multiplicar o valor do loop pela soma das operações internas dele
-        position += 1
-        end = array.index('FIMLOOP')
+      elif array[instruction] == 'PROCMULT':
+        amount += 10
+        instruction += 1
+
+      elif array[instruction] == 'LOOP':
+        position = instruction + 1; repetition = int(array[position])
+        position += 1; end = array.index('FIMLOOP')
         array[end] = 'was_an_end'
         # print('end', end)
+        # return the sum of its internal operations.
+        # slice the list in the loop operations: array[position:end]
         loop_complexity = sum_value_of_complexity(array[position:end])
         # print(array[position:])
-        amount += repetition * loop_complexity
-        position -= 2
-        end += 1
-        # del(array[ position:end ])
-        interaction += ( end - position )
-        # print(interaction)
+        amount += repetition * loop_complexity # multiply the loop value by the sum of its internal operations.
+        # On the next iteration, the position will be jumped to the next position after the 'LOOPFIM'
+        position -= 2; end += 1; instruction += ( end - position )
+        # print(instruction)
 
       else:
         return 'error'
-      # print('interaction', interaction)
-      # rep_for += 1
-      
-      
-
+      # print('instruction', instruction)
 
   else:
     pass
 
   return amount
+
 
 def main():
   algorithm_list = input().split() # 
