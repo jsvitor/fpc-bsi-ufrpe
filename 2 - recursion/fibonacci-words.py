e057ccb4199a6188ad80a87492888a7325e74f68
Vitor <jszvitor@gmail.com>
2021-09-16 10:35:23 -0300

    refactor(main): removed some indentations

--- 2 - recursion/fibonacci-words.py
+++ 2 - recursion/fibonacci-words.py
@@ -1,7 +1,7 @@
 """
 Fundamentals of Computational Problems I -
 Information System at Rural Federal University of Pernambuco
-Student: jsvitor
+Student: https://github.com/jsvitor
 
 problem:
 f(n) = “b”, se n = 0
@@ -11,19 +11,17 @@ f(n) = f(n-1) + f(n-2), se n > 1
 
 def words_of_fib(n):
   # n: nth term
-  if n == 0:
-    return 'b'
-  elif n == 1:
-    return 'a'
-  else:
-    return  f'{words_of_fib( n - 1 )}{words_of_fib( n - 2 )}'
+  if n == 0: return 'b'
+  
+  elif n == 1: return 'a'
+  
+  else: return  f'{words_of_fib( n - 1 )}{words_of_fib( n - 2 )}'
 
 def main():
   nth_term = int(input())
   print(words_of_fib(nth_term))
 
-if __name__ == '__main__':
-  main()
+if __name__ == '__main__': main()
 
 # print(words_of_fib(0)) # output: b
 # print(words_of_fib(3)) # output: aba
