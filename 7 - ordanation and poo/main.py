9c9460784ecac56bd51a71193a1920b419bd9163
Vitor <jszvitor@gmail.com>
2021-10-26 16:05:27 -0300

    feat(main): added entrance treatment

--- 7 - ordanation and poo/main.py
+++ 7 - ordanation and poo/main.py
@@ -1,8 +1,114 @@
+# classes Biblioteca e Livro:
+# - A Biblioteca contem uma lista de livros disponiveis e uma lista de livros alugados
+# - A biblioteca possui um metodo para alugar um livro. Caso o livro jah esteja alugado a pessoa nao poderah alugar este livro.
+# - A biblioteca possui um metodo para devolver o livro.
+# - A biblioteca possui um metodo que devolve o nome do livro mais alugado.
+
+class Livro:
+    codigo = None
+    nome = None
+    autor = None
+    __qtdeAlugueis = 0
+
+    def __init__(self, codigo, nome, autor):
+        self.codigo = codigo
+        self.nome = nome
+        self.autor = autor
+        
+
+        
+    def incrementaAluguel(self):
+        self.__qtdeAlugueis += 1
+    def getQtdeAlugueis(self):
+        return self.__qtdeAlugueis
+
+class Biblioteca:
+    alugados = []
+    disponiveis = []
+
+    def inserir(self, livro):
+        self.disponiveis.append(livro)
+
+    def alugar(self, livro):
+        ok = True
+        mensagem = None
+        if livro in self.disponiveis:
+            for i in self.disponiveis:
+                if i == livro:
+                    i.incrementaAluguel()
+                    self.alugados.append(i)
+                    self.disponiveis.remove(i)
+                    break
+        elif livro in self.alugados:
+            ok = False
+            mensagem = "O livro ja esta alugado, infelizmente voce nao podera alugar"
+        else:
+            ok = False
+            mensagem = "O livro nao existe"
+        return (ok, mensagem)
+
+    def devolver(self, codLivro):
+        ok = True
+        mensagem = None
+        for livro in self.alugados:
+            if livro.codigo == codLivro:
+                self.disponiveis.append(livro)
+                self.alugados.remove(livro)
+                break
+        else:
+            ok = False
+            mensagem = "O livro nao esta alugado"
+        return (ok, mensagem)
+
+    def livroMaisAlugado(self):
+        ok = True
+        mensagem = None
+        maior = 0
+        nome = None
+        for livro in self.disponiveis:
+            if livro.getQtdeAlugueis() > maior:
+                maior = livro.getQtdeAlugueis()
+                nome = livro.nome
+        for livro in self.alugados:
+            if livro.getQtdeAlugueis() > maior:
+                maior = livro.getQtdeAlugueis()
+                nome = livro.nome
+        if maior == 0:
+            ok = False
+            mensagem = "Nenhum livro foi alugado ainda"
+        else:
+            mensagem = "O livro mais alugado e: %s (%d alugueis)"%(nome, maior)
+            return (ok, mensagem)
 
 
 
 def main():
-  pass
+    """ 
+        Entrada: qnt, código,nome,autor
+        3,234,Fortaleza Digital,Dan Brown,423,Calculo 1,Stwart,156,Fisica,Tipler
+
+        Saída:
+        423 156 234
+    """
+
+    entry = input().split(',')
+    books = entry[1:] # recorta apenas os livros
+
+    # criando um objeto do tipo Biblioteca:
+    library = Biblioteca()
+
+    ## inserindo os objetos do tipo Livro num objeto do tipo Biblioteca
+    count = 0
+    while count < int(entry[0]):
+        cod, name, author = books[0:3]
+        book = Livro(cod, name, author)
+        
+        # inserindo livro na biblioteca
+        library.inserir(book)
+        del(books[0:3])
+        count += 1
+
+    
 
 
 if __name__ == '__main__':
